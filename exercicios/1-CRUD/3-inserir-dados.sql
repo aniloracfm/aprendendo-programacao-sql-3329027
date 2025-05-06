52d2f0a19da06542b6e9176f162efe5f6a32ebbf
Carolina Machado <77412256+aniloracfm@users.noreply.github.com>
2025-05-06 19:47:11 -0300

    add coluna gerencia

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -2,7 +2,8 @@
 INSERT INTO 
   salario_classe(id,salario,nivel)
 VALUES
-  (1,3500, 'estagiária'),
-  (2,4700, 'iniciante - grade 1'),
-  (3,5300, 'iniciante - grade 2');
+--  (1,3500, 'estagiária'),
+--  (2,4700, 'iniciante - grade 1'),
+--  (3,5300, 'iniciante - grade 2'),
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+  (8500, 'gerencia');
