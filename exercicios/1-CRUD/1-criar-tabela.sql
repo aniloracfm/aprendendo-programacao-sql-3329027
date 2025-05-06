f41e19a297374485e9e5488563f24779a7f482eb
Carolina Machado <77412256+aniloracfm@users.noreply.github.com>
2025-05-06 19:29:41 -0300

    exercicios

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,5 +1,5 @@
 -- Crie uma nova tabela no banco de dados
-CREATE TABLE salario_casse(
+CREATE TABLE salario_classe(
   id      INTEGER PRIMARY KEY AUTOINCREMENT,
   nivel   TEXT,
   salario INTEGER
