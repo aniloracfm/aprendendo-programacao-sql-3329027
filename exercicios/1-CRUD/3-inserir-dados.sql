e927f7b74d083b8b7692f9bb4998737891b17fa9
Carolina Machado <77412256+aniloracfm@users.noreply.github.com>
2025-05-06 19:48:13 -0300

    add gerencia

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,6 +1,6 @@
 -- Insira 3 classes salariais na tabela salario_classe
 INSERT INTO 
-  salario_classe(id,salario,nivel)
+  salario_classe(salario,nivel)
 VALUES
 --  (1,3500, 'estagiária'),
 --  (2,4700, 'iniciante - grade 1'),
