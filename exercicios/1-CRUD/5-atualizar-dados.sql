5c4cdc6f64952eac431deab63dcfd91d4f11fd1a
Carolina Machado <77412256+aniloracfm@users.noreply.github.com>
2025-05-06 20:14:08 -0300

    update - id_salary is null

--- exercicios/1-CRUD/5-atualizar-dados.sql
+++ exercicios/1-CRUD/5-atualizar-dados.sql
@@ -4,13 +4,19 @@ SET id_salario = 4
 WHERE EmployeeId = 6;*/
 
 -- Atualize a classe salarial e o LastName da pessoa cujo EmployeeId é igual a 2. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
-UPDATE 
+/*UPDATE 
   employees
 SET 
   id_salario = 4,
   LastName = 'Machado'
 WHERE
-  EmployeeId = 2;
+  EmployeeId = 2;*/
 
 
 -- Atualize a classe salarial 1 para as pessoas cuja coluna id_salario continua NULL. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
+UPDATE 
+  employees
+SET 
+  id_salario = 1
+WHERE
+  id_salario IS NULL;
